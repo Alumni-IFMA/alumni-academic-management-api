d94984604700c941be4be97fb7a125890114f5e8
Kênia <128853726+keniareis@users.noreply.github.com>
2026-03-01 13:33:19 -0300

    remove unused import

--- src/test/java/com/alumni/academic_management_api/repository/UserRepositoryTest.java
+++ src/test/java/com/alumni/academic_management_api/repository/UserRepositoryTest.java
@@ -4,9 +4,7 @@ import com.alumni.academic_management_api.entity.User;
 import com.alumni.academic_management_api.enums.AccountStatus;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 

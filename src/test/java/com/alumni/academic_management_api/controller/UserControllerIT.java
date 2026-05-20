9f4d714bf1de24738ff3d7c8b1e59a269312c182
marcos-valle <marcos.vallesn@gmail.com>
2026-05-20 19:26:00 -0300

    style: remove unused import in UserControllerIT

--- src/test/java/com/alumni/academic_management_api/controller/UserControllerIT.java
+++ src/test/java/com/alumni/academic_management_api/controller/UserControllerIT.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
